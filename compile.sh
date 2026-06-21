db37a673c0f08ae40d8732223a986d954253e06a
Márcio Castelo Branco <48799029+marcioclay@users.noreply.github.com>
2026-06-21 17:12:51 -0300

    Update clang command to include architecture-specific includes

--- compile.sh
+++ compile.sh
@@ -8,7 +8,7 @@ echo "Compiling eBPF program using Docker..."
 sudo docker run --rm -v $(pwd):/code -w /code ubuntu:22.04 /bin/bash -c "
     apt-get update && \
     apt-get install -y clang llvm libbpf-dev gcc-multilib && \
-    clang -O2 -g -target bpf -c xdp_monitor.c -o xdp_monitor.o
+    clang -g -O2 -target bpf -I/usr/include/$(uname -m)-linux-gnu -c xdp_monitor.c -o xdp_monitor.o
 "
 
 if [ -f xdp_monitor.o ]; then
